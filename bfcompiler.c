446788a49640461e0e940881f3dde611d186f72c
cameron <cameron@debianVM>
2017-01-24 12:35:00 -0500

    wip

--- bfcompiler.c
+++ bfcompiler.c
@@ -1,4 +1,48 @@
 #include "bfcompiler.h"
-#include <stdio.h>
 
-void main(int argc, char *argv[]) {}
+static error_t parse_opt(int key, char *arg, struct argp_state *state) {
+	/* Get the input argument from argp_parse, which we
+	   know is a pointer to our arguments structure. */
+	struct arguments *arguments = state->input;
+
+	switch (key) {
+		case 'q':
+		case 's': arguments->silent		 = 1; break;
+		case 'v': arguments->verbose	 = 1; break;
+		case 'o': arguments->output_file = arg; break;
+
+		case ARGP_KEY_ARG:
+			if (state->arg_num >= 2) /* Too many arguments. */
+				argp_usage(state);
+
+			arguments->args[state->arg_num] = arg;
+
+			break;
+
+		case ARGP_KEY_END:
+			if (state->arg_num < 2) /* Not enough arguments. */
+				argp_usage(state);
+			break;
+
+		default: return ARGP_ERR_UNKNOWN;
+	}
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	struct arguments arguments;
+
+	/* Default values. */
+	arguments.silent	  = 0;
+	arguments.verbose	 = 0;
+	arguments.output_file = "-";
+
+	/* Parse our arguments; every option seen by parse_opt will
+	   be reflected in arguments. */
+	argp_parse(&argp, argc, argv, 0, 0, &arguments);
+
+	printf("ARG1 = %s\nARG2 = %s\nOUTPUT_FILE = %s\n"
+		   "VERBOSE = %s\nSILENT = %s\n",
+		   arguments.args[0], arguments.args[1], arguments.output_file,
+		   arguments.verbose ? "yes" : "no", arguments.silent ? "yes" : "no");
+}
--- bfcompiler.h
+++ bfcompiler.h
@@ -1,6 +1,33 @@
 #ifndef BFCOMPILER_H_
 #define BFCOMPILER__H_
 
-#define SYSTEM_LINUX 0x03
+#include <argp.h>
+#include <elf.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static error_t parse_opt(int, char *, struct argp_state *);
+
+const char *argp_program_version     = "bfcompiler 0.1";
+const char *argp_program_bug_address = "<cameronswinoga@gmail.com>";
+static char doc[]		     = "Documentation";
+static char args_doc[]		     = "ARG1 ARG2";
+
+/* The options we understand. */
+static struct argp_option options[] = {
+    {"verbose", 'v', 0, 0, "Produce verbose output"},
+    {"quiet", 'q', 0, 0, "Don't produce any output"},
+    {"silent", 's', 0, OPTION_ALIAS},
+    {"output", 'o', "FILE", 0, "Output to FILE instead of standard output"},
+    {0}};
+
+struct arguments {
+  char *args[2]; /* arg1 & arg2 */
+  int   silent, verbose;
+  char *output_file;
+};
+
+static struct argp argp = {options, parse_opt, args_doc, doc};
 
 #endif // BFCOMPILER_H_
