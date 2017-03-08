3d094ab137b1d469a1ee6390697e904fcc863efb
Cameron Swinoga <swinogca@mcmaster.ca>
2017-03-08 10:49:28 -0500

    Moved around functions

--- helpers.c
+++ helpers.c
@@ -8,3 +8,20 @@ void debugPrintf(const char *fmt, ...) {
 		va_end(args);
 	}
 }
+
+uint32_t stringIndexFromSectionIndex(uint8_t stringData[], uint8_t index) {
+	uint32_t stringIndex = 0;
+	uint16_t i;
+	for (i = 0; stringIndex != index; i++)
+		if (*(stringData + i) == '\0') stringIndex++;
+	return i;
+}
+
+void addSectionData(SECTION *section, uint8_t startByte[], uint16_t size) {
+	(*section).bytes = realloc((*section).bytes, ((*section).size + size) * sizeof(uint8_t));
+
+	uint16_t byteIndex = 0;
+	for (uint16_t i = (*section).size; i < (*section).size + size; i++) (*section).bytes[i] = *(startByte + byteIndex++);
+
+	(*section).size += size;
+}
--- helpers.h
+++ helpers.h
@@ -2,11 +2,15 @@
 #define HELPERS_H_
 
 #include "arguments.h"
+#include "elfHelper.h"
 #include <stdarg.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-void debugPrintf(const char *, ...);
+void     debugPrintf(const char *, ...);
+uint32_t stringIndexFromSectionIndex(uint8_t *, uint8_t);
+void     addSectionData(SECTION *, uint8_t *, uint16_t);
 
 #endif // HELPERS_H_
