cd2a4dd586b01eaff83e6e7d6c6fa3c372a82aaf
Julianne Swinoga <julianneswinoga@gmail.com>
2017-03-21 11:57:20 -0400

    Setting up assembly files

---
+++ assembly.c
@@ -0,0 +1,17 @@
+#include "assembly.h"
+
+// INC (FF-0 r/m16/32/64)
+void construct_INC() {
+}
+
+// DEC (FF-1 r/m16/32/64)
+void construct_DEC() {
+}
+
+// ADD 4, esp (01-r r/m16/32/64 r16/32/64)
+void construct_ADDESP() {
+}
+
+// SUB 4, esp (29-4 r/m16/32/64 r16/32/64)
+void construct_SUBESP() {
+}
