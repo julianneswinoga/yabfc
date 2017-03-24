b92a3fae8d457c86251c3913d0af87c653796fde
Cameron Swinoga <cameronswinoga@gmail.com>
2017-03-24 17:21:28 -0400

    WIP

--- assembly.h
+++ assembly.h
@@ -2,15 +2,31 @@
 #define ASSEMBLY_H_
 
 #include "arguments.h"
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef struct {
+	int      size;
+	uint8_t *bytes;
+} CODE;
 
 // Function declarations
-void construct_INC();
+void construct_arbitrary(CODE *, uint8_t *, uint16_t);
+void construct_INC(CODE *);
+void construct_DEC();
+void construct_ADDESP();
+void construct_SUBESP();
+void construct_LPSTART();
+void construct_LPEND();
+void construct_INPUT();
+void construct_PRINT(CODE *);
+void construct_END(CODE *);
 
 /*
-+	INC (FF-0 r/m16/32/64)
--	DEC (FF-1 r/m16/32/64)
-<	ADD 4, esp (01-r r/m16/32/64 r16/32/64)
->	SUB 4, esp (29-4 r/m16/32/64 r16/32/64)
++	INC [esp] (FF-0 r/m16/32/64)
+-	DEC [esp] (FF-1 r/m16/32/64)
+>	ADD 4, esp (01-r r/m16/32/64 r16/32/64)
+<	SUB 4, esp (29-4 r/m16/32/64 r16/32/64)
 [
 ]
 ,
